f8094edd74179568dff6175ba0585ba65c010e4a
mertensblackfyre <72759681+mertensblackfyre@users.noreply.github.com.>
2025-11-27 23:01:39 +0300

    nice

--- includes/parser.h
+++ includes/parser.h
@@ -2,10 +2,15 @@
 
 #include "helper.h"
 #include "spdlog/spdlog.h"
+#include <array>
 #include <fmt/base.h>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <string_view>
+#include <variant>
+#include <vector>
+#include <algorithm>
 
 struct ParserMemory {
   std::string value{};
@@ -14,10 +19,14 @@ struct ParserMemory {
 };
 
 class Parser {
-public:
+private:
   std::string line;
   std::ifstream input_file;
+  std::array<std::string_view, 9> logical_commands = {
+      "add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"};
 
+public:
+  std::vector<std::variant<std::string_view, ParserMemory>> commands;
   Parser(const std::string &file_name) {
     input_file.open(file_name);
     if (!input_file.is_open())
@@ -37,9 +46,12 @@ public:
 void Parser::parse_read_file() {
   while (std::getline(input_file, line)) {
     std::string ll = parse_instruction(line);
+    auto it = std::find(logical_commands.begin(), logical_commands.end(), std::to_string_view(ll));
     if (helper_get_first_word(ll) == "push" ||
         helper_get_first_word(ll) == "pop") {
-      parse_memory_commands(ll);
+      commands.emplace_back(parse_memory_commands(ll));
+    } else if (it != std::end(logical_commands)) {
+      commands.emplace_back(ll);
     }
   };
 };
--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 #include "spdlog/spdlog.h"
 #include <fmt/base.h>
 #include <string>
+#include <string_view>
 
 int main(int argc, char **argv) {
 
@@ -15,5 +16,17 @@ int main(int argc, char **argv) {
 
   Parser parse(input);
   parse.parse_read_file();
+
+  for (auto a : parse.commands) {
+    // Access the int
+    if (std::holds_alternative<std::string_view>(a)) {
+      fmt::print("{} ", std::get<std::string_view>(a));
+    }
+    if (std::holds_alternative<ParserMemory>(a)) {
+      fmt::print("{} ", std::get<ParserMemory>(a).destination);
+      fmt::print("{} ", std::get<ParserMemory>(a).command);
+      fmt::println("{}", std::get<ParserMemory>(a).value);
+    }
+  }
   return 0;
 }
